782f6e7dec212cd85a618ba5374ee8d6e8fb0fbc
Tasha Natasha <0955712824n@gmail.com>
2024-02-23 22:12:22 +0200

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/Navbar";
 import s from "./page.module.css";
-import Main from "./components/Main";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <main className={s.container}>
       <Navbar />
-      <Main />
+      {/* <Main /> */}
     </main>
   );
 }
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import s from "./Navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className={s.nav}>
+      <Link href="/work" className={s.nav_link}>
+        Work
+      </Link>
+      <Link href="/" className={s.nav_logo}>
+        <Image
+          src="/icons/logo3.svg"
+          style={{ objectFit: "contain" }}
+          alt="Logo"
+          // fill
+          // sizes="100vw"
+          width={200}
+          height={200}
+          priority
+        />
+      </Link>
+      <Link href="/profile" className={s.nav_link}>
+        Profile
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
